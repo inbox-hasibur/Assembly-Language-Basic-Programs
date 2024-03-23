e32111bfebf6432064ae7823514bf6848bec07af
Hasibur Rahman <113811769+inbox-hasibur@users.noreply.github.com>
2024-03-23 21:35:07 +0600

    Create Assembly_Basic_Programs.asm

---
+++ Assembly_Basic_Programs.asm
@@ -0,0 +1,31 @@
+;Display message on new line
+.MODEL SMALL
+.STACK 100H
+.DATA
+  CR EQU 0DH
+  LF EQU 0AH
+  MSG1 DB 'FIRST $',
+  MSG2 DB  CR,LF,'SECOND $'
+  MSG3 DB  CR,LF,'THIRD $'
+  MSG4 DB  CR,LF,'FOURTH $'
+.CODE
+  MAIN PROC
+    MOV AX, @DATA   ; Segment address of data segment
+    MOV DS, AX      ; Set DS to data segment
+    LEA DX, MSG1
+    MOV AH, 9
+    INT 21H
+    LEA DX, MSG2
+    MOV AH, 9
+    INT 21H
+    LEA DX, MSG3
+    MOV AH, 9
+    INT 21H
+    LEA DX, MSG4
+    MOV AH, 9
+    INT 21H
+    ; Terminate
+    MOV AH, 4CH     ; Interrupt 21h function 4ch for program termination
+        INT 21H     ; Call interrupt 21h
+  MAIN ENDP
+      END MAIN
