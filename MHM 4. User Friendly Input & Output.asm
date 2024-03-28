21e8ed003f7cae05d4dbf7ef41a9801dabb59a06
Hasibur Rahman <113811769+inbox-hasibur@users.noreply.github.com>
2024-03-28 21:26:18 +0600

    Create MHM 4. User Friendly Input & Output.asm

---
+++ MHM 4. User Friendly Input & Output.asm
@@ -0,0 +1,28 @@
+; MHM 4. User Friendly Input & Output
+.MODEL SMALL
+.STACK 100H
+.DATA
+    MSG1 DB 'Enter a number: $'
+    MSG2 DB 0DH,0AH,'Number: '
+    T1   DB ?,'$'
+.CODE
+    MAIN PROC
+        MOV AX, @DATA    ; INITIATE
+        MOV DS, AX
+        
+        MOV AH, 9        ; DISPLAY 1
+        LEA DX, MSG1
+        INT 21H
+        
+        MOV AH, 1        ; INPUT
+        INT 21H
+        MOV T1, AL
+        
+        MOV AH, 9        ; DISPLAY 2 + OUTPUT
+        LEA DX, MSG2
+        INT 21H
+        
+        MOV AH, 4CH      ; TERMINATE
+        INT 21H
+        MAIN ENDP
+    END MAIN
